e5a5b73095efe111ef2879d9efbc76e1884a6619
Josmarvs2 <u201611746@upc.edu.pe>
2024-04-23 17:13:07 -0500

    new test of report added

--- src/test/java/Report.java
+++ src/test/java/Report.java
@@ -1,3 +1,4 @@
+import com.tp.neuralscan.patient.model.ImageEntity;
 import com.tp.neuralscan.patient.model.PatientEntity;
 import com.tp.neuralscan.patient.model.ReportEntity;
 import io.cucumber.java.en.Given;
@@ -7,8 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -203,9 +203,104 @@ public class Report {
         List<ReportEntity> allReports = response.getBody();
         assertNotNull(allReports);
 
-
-
     }
+
+    /*
+    private String idpatient;
+    private String iddoctor;
+    @Given("I doctor {string} want to create a report by patient {string}")
+    public void i_doctor_want_to_create_a_report_by_patient(String iddoctor, String idpatient) {
+        this.iddoctor=iddoctor;
+        this.idpatient=idpatient;
+    }
+
+
+    @When("I create a new report with {string}, {string}, {string}, {string} and {string}")
+    public void i_create_a_new_report_with_and(String summary, String description, String comment, String path, String added) {
+        String postUrl = url + "report/doctor/"+iddoctor+"/patient/"+idpatient+"/create";
+
+        ImageEntity newImage = new ImageEntity();
+        ReportEntity newReport = new ReportEntity();
+
+        newReport.setSummary(summary);
+        newReport.setDescription(description);
+        newReport.setComment(comment);
+
+        newImage.setPath(path);
+        newImage.setAdded(added);
+        newReport.setImageEntity(newImage);
+
+        ReportEntity report = restTemplate.postForObject(postUrl,newReport, ReportEntity.class);
+
+        assertNotNull(report);
+    }
+
+
+    @Then("the system create a new report {string}")
+    public void the_system_create_a_new_report(String summary) {
+        String getUrl = url + "report";
+
+        ResponseEntity<List<ReportEntity>> response = restTemplate.exchange(
+                getUrl,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<ReportEntity>>() {}
+        );
+
+        List<ReportEntity> allReport = response.getBody();
+        assertNotNull(allReport);
+        assertFalse(allReport.isEmpty());
+
+        ReportEntity lastReport = allReport.get(allReport.size() - 1);
+        assertEquals(summary,lastReport.getSummary());
+    }
+
+
+*/
+
+    private String docId;
+    private String patId;
+
+    private ResponseEntity<String> response;
+
+    @Given("a doctor with ID {string} and a patient with ID {string}")
+    public void un_médico_con_ID_y_un_paciente_con_ID(String docId, String patId) {
+        this.docId = docId;
+        this.patId = patId;
+    }
+
+
+    @When("a POST request is sent to create a medical report")
+    public void se_envía_una_solicitud_POST_para_crear_un_informe_médico() {
+        // Construir la URL con los IDs del médico y el paciente
+        String postUrl = url + "report/doctor/" + doctorId + "/patient/" + patientId + "/create";
+
+        // Construir el cuerpo de la solicitud (en tu caso, deberías construir el JSON)
+        String jsonInputString = "{\"summary\":\"nuevo informe\",\"description\":\"Descripción detallada del nuevo\",\"comment\":\"Comentarios adicionales\",\"image\":{\"path\":\"C:/Users/user/Pictures/Melosita\",\"added\":\"Logo.png\"}}";
+
+        // Configurar la solicitud HTTP
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<>(jsonInputString, headers);
+
+        // Realizar la solicitud POST
+        RestTemplate restTemplate = new RestTemplate();
+        response = restTemplate.exchange(postUrl, HttpMethod.POST, entity, String.class);
+    }
+
+
+    @Then("an successful response with status code {int} is expected")
+    public void se_espera_una_respuesta_exitosa_con_código_de_estado(Integer expectedStatusCode) {
+        assertEquals(expectedStatusCode, response.getStatusCodeValue());
+    }
+
+
+    @Then("the medical report is created correctly")
+    public void el_informe_médico_se_crea_correctamente() {
+        String responseBody = response.getBody();
+    }
+
+
 
 
 }
